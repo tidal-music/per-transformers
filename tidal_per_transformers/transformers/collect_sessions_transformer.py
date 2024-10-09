7e803695e5b34a7fed45697e626642798c8d4799
Ariel Fabiano <127113943+sqafabiano@users.noreply.github.com>
2024-10-09 17:51:40 +0100

    PER-4777 Fix with split logic and simplified selection

--- tidal_per_transformers/transformers/collect_sessions_transformer.py
+++ tidal_per_transformers/transformers/collect_sessions_transformer.py
@@ -1,14 +1,13 @@
-from pyspark.sql import functions as F
+from pyspark.sql import functions as F, DataFrame
 from pyspark.sql.window import Window
 from tidal_per_transformers.transformers import LoggableTransformer
 
 
 class CollectSessionsTransformer(LoggableTransformer):
     """
-    Given a group of scored and time-sorted sessions by the users, it will collect them into 2 lists:
-    - Recent sessions: last X streamed tracks, with last listened item in last position
-    - Relevant sessions: grouping of all sessions ordered by their score, with the top session last and its first song in last position
-    Both lists will be trimmed to the given limit.
+    Given a group of scored and time-sorted sessions by the users, it will collect
+    them into 2 lists: recent sessions and relevant sessions. Both lists will be
+    trimmed to the given limit.
 
     :param group_by: Columns to group the DataFrame by
     :param order_by_session: The column used to sort the values by time
@@ -36,38 +35,59 @@ class CollectSessionsTransformer(LoggableTransformer):
         self.list_col = list_col
         self.limit = limit
 
-    def _transform(self, dataset):
-        window_score = Window.partitionBy(self.group_by).orderBy(
-            F.desc(F.col(self.order_by_score))
-        )
-        window_session = Window.partitionBy(self.group_by).orderBy(F.col(self.order_by_session))
+    def _group_collect_recency_ordered(self, dataset: DataFrame) -> DataFrame:
+        """
+        Recent streaming sessions are composed of last X streamed tracks, with last
+        listened item in last position.
 
-        return (
-            dataset.withColumn(
-                self.recent_alias,
-                F.reverse(
-                    F.slice(
-                        F.reverse(
-                            F.flatten(
-                                F.collect_list(self.list_col).over(window_session)
-                            )
-                        ),
-                        1, self.limit,
-                    )
-                ),
-            )
-            .withColumn(
-                self.relevant_alias,
-                F.reverse(
-                    F.slice(
-                        F.flatten(F.collect_list(self.list_col).over(window_score)),
-                        1, self.limit,
-                    )
-                ),
-            )
-            .groupBy(self.group_by)
-            .agg(
-                F.max(self.recent_alias).alias(self.recent_alias),
-                F.max(self.relevant_alias).alias(self.relevant_alias),
-            )
+        This gets built by going through user's sessions on an ascending fashion (most recent
+        processed last) and incrementally collecting items, to then fetch the resulting list
+        from the last instance.
+
+        Note: due to the usage of F.last(), this function is not deterministic
+        """
+        window_session = Window.partitionBy(self.group_by).orderBy(F.col(self.order_by_session))
+        return dataset.withColumn(
+            self.recent_alias,
+            F.reverse(
+                F.slice(
+                    F.reverse(
+                        F.flatten(
+                            F.collect_list(self.list_col).over(window_session)
+                        )
+                    ),
+                    1, self.limit,
+                )
+            ),
+        ).groupBy(F.col(self.group_by)).agg(
+            F.last(F.col(self.recent_alias)).alias(self.recent_alias)
         )
+
+    def _group_collect_relevance_ordered(self, dataset: DataFrame) -> DataFrame:
+        """
+        Relevant streaming sessions are composed by grouping of all sessions ordered by
+        their relevance score, with the top session last and its first song in last position.
+
+        This gets built by going through scored user's sessions descending in relevance (lowest
+        scored processed last) and incrementally collecting items, to then fetch the resulting
+        list from the last instance.
+
+        Note: due to the usage of F.last(), this function is not deterministic
+        """
+        window_score = Window.partitionBy(self.group_by).orderBy(F.desc(F.col(self.order_by_score)))
+        return dataset.withColumn(
+            self.relevant_alias,
+            F.reverse(
+                F.slice(
+                    F.flatten(F.collect_list(self.list_col).over(window_score)),
+                    1, self.limit,
+                )
+            )
+        ).groupBy(F.col(self.group_by)).agg(
+            F.last(F.col(self.relevant_alias)).alias(self.relevant_alias)
+        )
+
+    def _transform(self, dataset: DataFrame) -> DataFrame:
+        recent = self._group_collect_recency_ordered(dataset)
+        relevant = self._group_collect_relevance_ordered(dataset)
+        return recent.join(relevant, on=self.group_by)
