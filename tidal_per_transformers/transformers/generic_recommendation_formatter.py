4717705b7f7741fe19515e19db30c1fd1b59f730
Ariel Fabiano <afabiano@squareup.com>
2024-10-10 16:15:13 +0100

    PER-4819 Adding posexplode method for extra measures

--- tidal_per_transformers/transformers/generic_recommendation_formatter.py
+++ tidal_per_transformers/transformers/generic_recommendation_formatter.py
@@ -1,4 +1,4 @@
-from pyspark.sql import functions as F, DataFrame
+from pyspark.sql import functions as F, DataFrame, Window
 from tidal_per_transformers.transformers.utils import constants as c
 from tidal_per_transformers.transformers import LoggableTransformer
 
@@ -10,13 +10,13 @@ class GenericRecommendationFormatter(LoggableTransformer):
     """
 
     def __init__(self,
-                 hash_key: str,
+                 group_key: str,
                  recommendations_col: str,
                  item_type: str,
                  module_name: str,
                  module_version: str):
         super(GenericRecommendationFormatter, self).__init__()
-        self.hash_key = hash_key
+        self.group_key = group_key
         self.recommendations_col = recommendations_col
         self.item_type = item_type
         self.module_name = module_name
@@ -26,7 +26,11 @@ class GenericRecommendationFormatter(LoggableTransformer):
         return (dataset
                 .withColumn(c.TYPE, F.lit(self.item_type))
                 .withColumn(c.MODULE_ID, F.lit(f"{self.module_name}_v{self.module_version}"))
-                .withColumn(c.ID, F.explode(self.recommendations_col))
-                .groupby([F.col(self.hash_key).alias(c.HASH_KEY), c.MODULE_ID])
-                .agg(F.collect_list(F.struct(F.col(c.TYPE), F.col(c.ID))).alias(self.recommendations_col))
+                .select(F.col(self.group_key).alias(c.HASH_KEY), c.TYPE, c.MODULE_ID,
+                        F.posexplode(self.recommendations_col))
+                .withColumn(self.recommendations_col,
+                            F.collect_list(F.struct(F.col(c.TYPE), F.col("col").alias(c.ID))).over(
+                                Window.partitionBy().orderBy("pos")))
+                .groupby([c.HASH_KEY, c.MODULE_ID])
+                .agg(F.max(self.recommendations_col).alias(self.recommendations_col))
                 )
