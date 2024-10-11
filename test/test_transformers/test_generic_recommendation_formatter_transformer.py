8e8d80d004c9a255c7d4f35251472db26bff24e7
Ariel Fabiano <afabiano@squareup.com>
2024-10-11 10:04:59 +0100

    Renaming main function

--- test/test_transformers/test_generic_recommendation_formatter_transformer.py
+++ test/test_transformers/test_generic_recommendation_formatter_transformer.py
@@ -1,5 +1,6 @@
 from pyspark.sql.types import Row
-from tidal_per_transformers.transformers.generic_recommendation_formatter import GenericRecommendationFormatter
+from tidal_per_transformers.transformers.generic_recommendation_formatter_transformer import \
+    GenericRecommendationFormatterTransformer
 from pyspark_test import PySparkTest
 import tidal_per_transformers.transformers.utils.constants as c
 
@@ -12,17 +13,17 @@ class AggregateTransformerTest(PySparkTest):
             (2, [12, 10, 15]),
         ], [c.USER, c.ITEM])
 
-        output = GenericRecommendationFormatter(group_key=c.USER,
-                                                recommendations_col=c.ITEM,
-                                                item_type=c.TRACK_GROUP,
-                                                module_name=c.MODULE_ID,
-                                                module_version="1"
-                                                ).transform(dataset)
+        output = GenericRecommendationFormatterTransformer(group_key=c.USER,
+                                                           recommendations_col=c.ITEM,
+                                                           item_type=c.TRACK_GROUP,
+                                                           module_name=c.MODULE_ID,
+                                                           module_version="1"
+                                                           ).transform(dataset)
 
         self.assertEqual([
             Row(hashKey=1, moduleId=f'{c.MODULE_ID}_v1',
                 item=[Row(type='trackGroup', id=11), Row(type='trackGroup', id=15)]),
             Row(hashKey=2, moduleId=f'{c.MODULE_ID}_v1',
                 item=[Row(type='trackGroup', id=12), Row(type='trackGroup', id=10), Row(type='trackGroup', id=15)])
-            ],
+        ],
             output.collect())
